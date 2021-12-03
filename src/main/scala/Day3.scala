d5a918ae795229ccb025f2307ee61cc31efeba0e
themother <jacktowleretc@hotmail.com>
2021-12-03 13:26:51 +0000

    Day 3 refactoring

--- src/main/scala/Day3.scala
+++ src/main/scala/Day3.scala
@@ -4,25 +4,29 @@ class Day3 extends Day(3) {
   val numEntries = data.size
   val numLength = data(0).length
 
-  def getDigit(pos: Int): Int =
-    val b = data.map(x => x(pos)).count(_ == '1')
-    if b > numEntries / 2 then 1 else 0
-
   override def answer1(): Any =
-
-    val s = List.range(0, numLength).map(getDigit)
-    val s2 = s.map(x => if x == 1 then 0 else 1)
+    val s = List.range(0, numLength).map({ pos =>
+      val b = data.map(x => x(pos)).count(_ == '1')
+      if b > numEntries / 2 then 1 else 0
+    })
+    val s2 = s.map {
+      _ match
+        case 1 => 0
+        case 0 => 1
+    }
     Integer.parseInt(s.mkString, 2) * Integer.parseInt(s2.mkString, 2)
 
   override def answer2(): Any =
     def loop(data: List[String], pos: Int, func: (Int, Int) => Boolean): Int = {
-      if data.size == 1 then Integer.parseInt(data(0).mkString, 2)
+      if data.size == 1 then
+        Integer.parseInt(data(0).mkString, 2)
       else {
-        val ones = data.map(_(pos)).count(_ == '1')
-        val zeros = data.map(_(pos)).count(_ == '0')
+        val ones = data.map(_ (pos)).count(_ == '1')
+        val zeros = data.map(_ (pos)).count(_ == '0')
         val mostCommon = if func(ones, zeros) then '1' else '0'
-        loop(data.filter(_(pos) == mostCommon), pos + 1, func)
+        loop(data.filter(_ (pos) == mostCommon), pos + 1, func)
       }
     }
+
     loop(data, 0, _ >= _) * loop(data, 0, _ < _)
 }
