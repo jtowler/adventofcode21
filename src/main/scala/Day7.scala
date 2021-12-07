9e5e4c51ab588ce2e70d8dfda3774af4a1d22497
themother <jacktowleretc@hotmail.com>
2021-12-07 10:57:34 +0000

    Refactored Day 7

--- src/main/scala/Day7.scala
+++ src/main/scala/Day7.scala
@@ -2,17 +2,11 @@ class Day7 extends Day(7) {
 
   val data = readData().getLines().toList(0).split(",").map(_.toInt).toList
 
-  def fact(a: Int): Int = a match {
-    case n if n <= 1 => n
-    case n => n + fact(n - 1)
-  }
-
-  def answer(f: (Int, Int) => Int): Int = (data.min to data.max).map{i =>
-    data.map(x => f(x, i)).sum
+  def answer(f: Int => Int): Int = (data.min to data.max).map { i =>
+    data.map(x => f((x - i).abs)).sum
   }.min
 
-  override def answer1(): Any = answer((x, y) => (x-y).abs)
+  override def answer1(): Any = answer(identity)
 
-  override def answer2(): Any = answer((x, y) => fact((x-y).abs))
-
+  override def answer2(): Any = answer(a => (a * (a + 1)) / 2)
 }
