e096a1564d57f7b44e2790c62b2b5646d10b5f09
themother <jacktowleretc@hotmail.com>
2021-12-08 12:01:55 +0000

    Added bigboy input option

--- src/main/scala/Day.scala
+++ src/main/scala/Day.scala
@@ -1,4 +1,4 @@
-import scala.io.Source
+import scala.io.{BufferedSource, Source}
 
 trait Day(day: Int) {
 
@@ -6,11 +6,15 @@ trait Day(day: Int) {
 
   def answer2(): Any
 
-  def display() =
+  def display(): Unit =
     println(answer1())
     print(answer2())
 
-  def readData(test: Boolean = false) =
-    val prefix = if test then "_test" else ""
-    Source.fromFile(s"resources/day_$day$prefix.txt")
+  def readData(test: Boolean = false, bigboy: Boolean = false): BufferedSource =
+    val suffix = (test, bigboy) match {
+      case (_, true) => "_bigboy"
+      case (true, _) => "_test"
+      case _ => ""
+    }
+    Source.fromFile(s"resources/day_$day$suffix.txt")
 }
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,3 @@
 object Main extends App {
-  Day9().display()
+  Day8().display()
 }
