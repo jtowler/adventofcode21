961dbef961669e4c49facec7680e6fde6f3395d3
themother <jacktowleretc@hotmail.com>
2021-12-08 11:09:12 +0000

    Refactored Day 8

--- src/main/scala/Day8.scala
+++ src/main/scala/Day8.scala
@@ -7,21 +7,21 @@ class Day8 extends Day(8) {
   override def answer1(): Any = easy.map(line => line.count(List(2, 3, 4, 7) contains _.size)).sum
 
   def determineVals(disps: Array[List[Char]]): Map[List[Char], Char] =
-    val one = disps.filter(_.size == 2)(0)
-    val four = disps.filter(_.size == 4)(0)
-    val seven = disps.filter(_.size == 3)(0)
-    val eight = disps.filter(_.size == 7)(0)
+    val one = disps.find(_.size == 2).get
+    val four = disps.find(_.size == 4).get
+    val seven = disps.find(_.size == 3).get
+    val eight = disps.find(_.size == 7).get
 
     val sixes = disps.filter(_.size == 6)
     val fives = disps.filter(_.size == 5)
 
-    val three = fives.filter(disp => one.map(seg => disp.contains(seg)).min)(0)
-    val six = sixes.filterNot(disp => one.map(seg => disp.contains(seg)).min)(0)
-    val nine = sixes.filter(disp => disp.count(seg => four contains seg) == 4)(0)
-    val zero = sixes.filterNot(disp => disp == six || disp == nine)(0)
+    val three = fives.find(disp => one.map(disp contains _).min).get
+    val six = sixes.find(disp => !one.map(disp contains _).min).get
+    val nine = sixes.find(disp => disp.count(four contains _) == 4).get
+    val zero = sixes.find(disp => disp != six && disp != nine).get
 
-    val five = fives.filter(disp => disp != three && disp.count(seg => four contains seg) == 3)(0)
-    val two = fives.filterNot(disp => disp == five || disp == three)(0)
+    val five = fives.find(disp => disp != three && disp.count(four contains _) == 3).get
+    val two = fives.find(disp => disp != five && disp != three).get
 
     Map(one -> '1', two -> '2', three -> '3', four -> '4', five -> '5', six -> '6',
       seven -> '7', eight -> '8', nine -> '9', zero -> '0')
---
+++ src/main/scala/Day9.scala
@@ -0,0 +1,8 @@
+class Day9 extends Day(9) {
+
+  val data = readData().getLines().toList(0).split(",").map(_.toInt).toList
+  
+  override def answer1(): Any = ""
+
+  override def answer2(): Any = ""
+}
