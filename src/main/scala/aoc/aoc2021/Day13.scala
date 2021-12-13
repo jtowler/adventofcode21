6c40a6e8a788e924f0a1608920a7b069540eeebf
themother <jacktowleretc@hotmail.com>
2021-12-13 10:51:37 +0000

    Refactored Day 13

--- src/main/scala/aoc/aoc2021/Day13.scala
+++ src/main/scala/aoc/aoc2021/Day13.scala
@@ -2,21 +2,16 @@ package aoc.aoc2021
 
 import aoc.Day
 
-def getBoard(coords: List[(Int, Int)]): List[List[Boolean]] =
-  List.tabulate(coords.map(_._2).max + 1, coords.map(_.head).max + 1)((y, x) =>
-    if coords contains (x, y) then true else false)
-
-def dispCoord(coords: List[List[Boolean]]): Unit =
-  coords.foreach ( x => println(x.map(if _ then '#' else ' ').mkString))
-
 class Day13 extends Day(13, 2021) {
 
   private val data = readData().getLines().toList
   private val splitInd = data.indexWhere(_ contains ' ')
-  private val coords = data.slice(0, splitInd - 1).map{ coord =>
+  private val coords = data.slice(0, splitInd - 1).map { coord =>
     val ins = coord.split(" ").last.split(",")
     (ins.head.toInt, ins.last.toInt)
   }
+  private val grid = List.tabulate(coords.map(_._2).max + 1, coords.map(_.head).max + 1)((y, x) =>
+    if coords contains(x, y) then true else false)
   private val folds = data.slice(splitInd, data.length).map { line =>
     val ins = line.split(" ").last.split("=")
     (ins.head.head, ins.last.toInt)
@@ -33,8 +28,10 @@ class Day13 extends Day(13, 2021) {
       (normal zip folded).map((xs, ys) => (xs zip ys).map(_ | _))
 
   override def answer1(): Any =
-    fold(getBoard(coords), folds.head._1, folds.head._2).map(_.count(_ == true)).sum
+    fold(grid, folds.head._1, folds.head._2).map(_.count(_ == true)).sum
 
   override def answer2(): Any =
-    dispCoord(folds.foldLeft(getBoard(coords))((b, f) => fold(b, f._1, f._2)))
+    folds.foldLeft(grid)((b, f) => fold(b, f._1, f._2)).map {
+      _.map(if _ then '#' else ' ').mkString
+    }.mkString("\n")
 }
