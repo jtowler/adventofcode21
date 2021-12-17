3d47854b807cd37f5ac023c1ba2d9cbbae561b77
themother <jacktowleretc@hotmail.com>
2021-12-17 18:24:49 +0000

    Completed Day 17

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,4 +1,4 @@
-import aoc.aoc2021.{Day16 => Today}
+import aoc.aoc2021.{Day17 => Today}
 
 @main
 def main(): Unit = Today().display()
--- src/main/scala/aoc/aoc2021/Day17.scala
+++ src/main/scala/aoc/aoc2021/Day17.scala
@@ -2,11 +2,49 @@ package aoc.aoc2021
 
 import aoc.Day
 
+import scala.annotation.tailrec
+
+//val (tX1: Int, tX2: Int, tY1: Int, tY2: Int) = (20, 30, -10, -5)
+val (tX1: Int, tX2: Int, tY1: Int, tY2: Int) = (79, 137, -176, -117)
+
+case class Probe(x: Int, y: Int, xVel: Int, yVel: Int) {
+  def update(): Probe =
+    val newXVel = xVel match {
+      case v if v < 0 => v + 1
+      case v if v > 0 => v - 1
+      case _ => xVel
+    }
+    Probe(x + xVel, y + yVel, newXVel, yVel - 1)
+  def inTarget: Boolean = (x >= tX1) && (x <= tX2) && (y >= tY1) && (y <= tY2)
+  def pastTarget: Boolean = y < tY1
+}
+
+case class Trajectory(probes: List[Probe]) {
+  def update(): Trajectory = Trajectory(probes.head.update() :: probes)
+  def inTarget: Boolean = probes.head.inTarget
+  def pastTarget: Boolean = probes.head.pastTarget
+  def maxY: Int = probes.map(_.y).max
+}
+object Trajectory {
+  def fromXY(x: Int, y: Int): Trajectory = loop(Trajectory(List(Probe(0, 0, x, y))))
+
+  @tailrec
+  private def loop(t: Trajectory): Trajectory = t match {
+    case trj if trj.pastTarget || trj.inTarget => trj
+    case _ => loop(t.update())
+  }
+}
+
 class Day17 extends Day(17, 2021) {
 
-  private val data = readData(true).getLines().toList
+  val bruteForce: Seq[Trajectory] = for {
+    i <- -200 to 200
+    j <- -200 to 200
+    t = Trajectory.fromXY(i, j)
+    if t.inTarget
+  } yield t
 
-  override def answer1(): Any = ""
+  override def answer1(): Any = bruteForce.maxBy(_.maxY).maxY
 
-  override def answer2(): Any = ""
+  override def answer2(): Any = bruteForce.size
 }
